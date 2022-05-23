7884be5988f6e1b6fd89cc0b4d596e4397df1966
Julien Chapron <jc271200001@protonmail.com>
2022-05-23 12:38:23 +0200

    [fix] add eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-    rules: {
-      "node/no-unsupported-features/es-syntax": [
-        "error",
-        {
-          version: ">=8.0.0",
-          ignores: ["vue", "axios", "vuetify"],
-        },
-      ],
-    },
-  };
+  rules: {
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      {
+        version: ">=8.0.0",
+        ignores: ["vue", "axios", "vuetify"],
+      },
+    ],
+  },
+};
